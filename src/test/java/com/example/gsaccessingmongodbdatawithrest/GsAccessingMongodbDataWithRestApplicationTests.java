685142f784e9f95219ac90b53e1d30fb0ff13c7a
BrookeBryski <brookeabryski@gmail.com>
2022-02-01 13:02:58 -0600

    initial commit

---
+++ src/test/java/com/example/gsaccessingmongodbdatawithrest/GsAccessingMongodbDataWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsaccessingmongodbdatawithrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingMongodbDataWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
